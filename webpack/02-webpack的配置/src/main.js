850575e3f1b53d2dff69e29533c9c12038b81027
邱福利 <qiufuli@jsb5.com>
2020-01-10 17:41:51 +0800

    '20011001'

--- webpack/02-webpack的配置/src/main.js
+++ webpack/02-webpack的配置/src/main.js
@@ -1,7 +1,18 @@
 // commonJS
-const {add} = require('./mathUtils');
+const {add} = require('./js/mathUtils');
 // es6 Module
-import {name} from './info'
+import {name} from './js/info'
+// css
+require('./css/index.css');
+require('./css/index2.less');
 
 add(20,30);
 console.log(name);
+
+import Vue from 'vue';
+ new Vue({
+  el:'#app',
+  data:{
+    mes:'hello Vue'
+  }
+})
--- webpack/02-webpack的配置/webpack.config.js
+++ webpack/02-webpack的配置/webpack.config.js
@@ -4,6 +4,45 @@ module.exports = {
   entry:'./src/main.js',
   output:{
     path:path.resolve(__dirname,'dist'),
-    filename:'bundle.js'
+    filename:'bundle.js',
+    // 任何涉及到url的都会加上publicPath
+    publicPath:'dist/'
+  },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      },
+      {
+        test:/\.less$/,
+        use:['style-loader','css-loader','less-loader']
+      },
+      {
+        test:/\.(png|jpg|gif)$/,
+        use:[{
+          loader:'url-loader',
+          options:{
+            limit:8192,
+            name:'img/[name]-[hash:8].[ext]'
+          },
+        }]
+      },
+      {
+        test:/\.js$/,
+        exclude:/(node_modules|bower_components)/,
+        use:{
+          loader:'babel-loader',
+          options:{
+            presets:['es2015']
+          }
+        }
+      }
+    ]
+  },
+  resolve:{
+    alias:{
+      'vue$':'vue/dist/vue.esm.js'
+    }
   }
 }
